e8a9ed1f9a68158398cf41954f04cf0001bb121b
polina-shimolina <shimolinapk@gmail.com>
2025-03-29 15:48:29 +0300

    add button to add chat

--- websocket-chat/src/components/ChatList.tsx
+++ websocket-chat/src/components/ChatList.tsx
@@ -1,23 +1,104 @@
-import { Box, List, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material"
-import { Dispatch, FC } from "react"
+import { Box, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, Fade } from "@mui/material"
+import { Dispatch, FC, useState } from "react"
 import { IChat } from "../interface/IChat";
-const ChatList: FC<{chats: IChat[]; setSelectedChat: Dispatch<React.SetStateAction<IChat | null>>}> = ({chats, setSelectedChat}) => {
+import AddIcon from '@mui/icons-material/Add';
+import GroupIcon from '@mui/icons-material/Group';
+import PersonIcon from '@mui/icons-material/Person';
 
+const ChatList: FC<{chats: IChat[]; setSelectedChat: Dispatch<React.SetStateAction<IChat | null>>}> = ({chats, setSelectedChat}) => {
+    const [hovered, setHovered] = useState(false);
+    
     const handleChooseChat = (chat: IChat) => {
         setSelectedChat(chat)
     }
 
+    const handleCreateGroup = () => {
+        console.log("Создать групповой чат");
+    }
+
+    const handleCreatePrivate = () => {
+        console.log("Создать личный чат");
+    }
+
     return (
         <Box sx={{marginY: 1}}>
-            <Typography sx={{fontWeight: 500, fontSize: 20 }}>Мои чаты</Typography>
             <List>
                 {chats.map((item: IChat) => 
-                    (<ListItem key={item.chatId} sx={{padding: 0}}>
-                        <ListItemButton onClick={() => handleChooseChat(item)}>
+                    (<ListItem key={item.chatId} sx={{padding: 0.5}}>
+                        <ListItemButton 
+                            onClick={() => handleChooseChat(item)}
+                            sx={{
+                                borderRadius: 2,
+                                backgroundColor: 'grey.100',
+                                '&:hover': {
+                                    backgroundColor: 'grey.200'
+                                }
+                            }}
+                        >
                             <ListItemText primary={item.title}/>
+                            {item.type === "group" && 
+                                <ListItemIcon sx={{ 
+                                    minWidth: 'auto',
+                                    marginLeft: 'auto'
+                                }}>
+                                    <GroupIcon/>
+                                </ListItemIcon>
+                            }
                         </ListItemButton>
                     </ListItem>))}
             </List>
+            <Box sx={{ 
+                display: 'flex', 
+                justifyContent: 'center',
+                position: 'relative',
+                height: 40
+            }}>
+                <Fade in={!hovered}>
+                    <IconButton 
+                        onMouseEnter={() => setHovered(true)}
+                        sx={{ 
+                            position: 'absolute',
+                            backgroundColor: 'grey.100',
+                            '&:hover': {
+                                backgroundColor: 'grey.200'
+                            }
+                        }}
+                    >
+                        <AddIcon/>
+                    </IconButton>
+                </Fade>
+
+                <Fade in={hovered}>
+                    <Box sx={{ display: 'flex', gap: 1 }}>
+                        <IconButton 
+                            onMouseLeave={() => setHovered(false)}
+                            onClick={handleCreatePrivate}
+                            sx={{ 
+                                backgroundColor: 'grey.100',
+                                '&:hover': {
+                                    backgroundColor: 'grey.200'
+                                }
+                            }}
+                            title="Создать личный чат"
+                        >
+                            <PersonIcon/>
+                        </IconButton>
+                        <IconButton 
+                            onMouseLeave={() => setHovered(false)}
+                            onClick={handleCreateGroup}
+                            sx={{ 
+                                backgroundColor: 'grey.100',
+                                '&:hover': {
+                                    backgroundColor: 'grey.200'
+                                }
+                            }}
+                            title="Создать групповой чат"
+                        >
+                            <GroupIcon/>
+                        </IconButton>
+                    </Box>
+                </Fade>
+            </Box>
         </Box>
     )
 }
